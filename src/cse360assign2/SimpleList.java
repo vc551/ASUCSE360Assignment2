cd7c60b8942741bab793850cbecec5265d3e6107
Vikramjit Singh <vikramjitsingh551@gmail.com>
2020-02-26 11:23:08 -0700

    Finished Step 1

--- src/cse360assign2/SimpleList.java
+++ src/cse360assign2/SimpleList.java
@@ -2,7 +2,7 @@
  * Name		  :	Vikramjit Singh
  * ASU ID	  :	1215398340
  * Assignment :	1
- * Description: This class defines the SimpleList data type for Assignment 1.
+ * Description: This class defines the SimpleList data type for Assignment 2.
  * 				Private variables include an array of integers called list
  * 				and an integer count which keeps track of the number of
  * 				variables stored into the array. The constructor sets the list
@@ -28,10 +28,18 @@ public class SimpleList
 
 	public void add(int toAdd)			//Add Method : Adds the element to the head of the array
 	{
+		if(this.list.length == this.count)											//Checks if array size is equal to the maximum size of the array
+		{
+			int newSize = this.list.length + ((this.list.length * 50)/ 100);		//Determine the new size of the array to be the original size and 50% of the size
+			int newArray[] = new int[newSize];										//Create a new array of the new determined size
+			for(int counter = 0 ; counter < this.count ; counter++)					//Copy the contents of the array into the new bigger array
+				newArray[counter] = this.list[counter];
+			this.list = newArray;													//Replace the old array with the new bigger array
+		}
 		for(int counter = this.list.length - 1 ; counter > 0 ; counter--) 			//Shifts the elements one place to the right
 			this.list[counter] = this.list[counter-1];
 		this.list[0] = toAdd;														//Adds element to the start of the array
-		this.count = Math.min(this.count + 1, 10);									//Changes count to be +1 but not more than 10
+		this.count++;																//Changes count to be +1
 	}
 
 	public void remove(int toRemove)	//Remove Method : Removes the given element from the list
@@ -42,6 +50,13 @@ public class SimpleList
 			for(int counter = removeIndex ; counter < this.count - 1 ; counter++) 	//Shifts elements to the left from the index to the end of the list
 				this.list[counter] = this.list[counter+1];
 			list[this.count--] = 0;													//Decrement count, and set tail to 0
+		}
+		if(this.count <= ((this.list.length * 75)/100))								//Check if the array is less than 75% full
+		{
+			int newArray[] = new int[this.count];									//Create a new array of size count
+			for(int counter = 0 ; counter < newArray.length ; counter++)			//Copy the contents of the array into the new smaller array
+				newArray[counter] = this.list[counter];
+			this.list = newArray;													//Replace the old array with the new smaller array
 		}
 	}
 
